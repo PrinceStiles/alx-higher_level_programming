b0319f312327211b394a05428902a62c440d445d
Prince <princekarpture@gmail.com>
2023-06-07 11:50:16 -0700

    print ANSCII, exclude q,e

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for letter in range(97, 123):
     if letter is not 101 and letter is not 113:
-        print(f"{letter:c}", end="")
+        print("{:c}".format(letter), end='')

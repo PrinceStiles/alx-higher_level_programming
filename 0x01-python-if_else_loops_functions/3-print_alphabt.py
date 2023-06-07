326dbe367600d2718fab7c225a5924dadd38dd4b
Prince <princekarpture@gmail.com>
2023-06-07 11:54:48 -0700

    print ASCII

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for letter in range(97, 123):
     if letter is not 101 and letter is not 113:
-        print("{:c}".format(letter), end='')
+        print("{}".format(chr(letter)), end="")

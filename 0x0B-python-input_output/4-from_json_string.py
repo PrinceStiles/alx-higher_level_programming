d5055f0b45b0ff54cd11021535ab3a0302fce1aa
PrinceStiles <princekarpture@gmail.com>
2024-02-26 11:37:07 -0800

    from_json_string

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-#4-from_json_string.py
+# 4-from_json_string.py
 
 """returns an object represented by json string"""
 
 import json
+
 
 def from_json_string(my_str):
     """do jsut that - shebang"""

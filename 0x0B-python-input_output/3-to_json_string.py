8f573ac74c6b903f4d8b9072c4dd68b9ed0dce6f
PrinceStiles <princekarpture@gmail.com>
2024-02-26 11:18:46 -0800

    to_json_string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+#3-to_json_string.py
+
+"""Function that returns json string"""
+
+import json
+
+def to_json_string(my_obj):
+    """returns json representation of an object"""
+
+    return json.dumps(my_obj)

88c006bbdb4e73b297b9d5138e494cedc0c9c224
PrinceStiles <princekarpture@gmail.com>
2024-02-26 10:44:19 -0800

    write_file

---
+++ 0x0B-python-input_output/write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+#1-write_file.py
+
+"""write a string to a text file"""
+
+def write_file(filename="", text=""):
+    """do that and return nb of characters"""
+
+    with open(filename, mode="w", encoding="utf-8") as f:
+        return f.write(text)
+
